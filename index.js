7cebd439bfeaec189bfc020dfea69da4a4c6554c
ismaelc <chrisi@13mbp-11334.gateway.pace.com>
2016-07-14 16:11:43 -0700

    test

--- index.js
+++ index.js
@@ -3,56 +3,60 @@ var Botkit = require('botkit')
 var token = process.env.SLACK_TOKEN
 
 var controller = Botkit.slackbot({
-  // reconnect to Slack RTM when connection goes bad
-  retry: Infinity,
-  debug: false
+    // reconnect to Slack RTM when connection goes bad
+    retry: Infinity,
+    debug: false
 })
 
 // Assume single team mode if we have a SLACK_TOKEN
 if (token) {
-  console.log('Starting in single-team mode')
-  controller.spawn({
-    token: token
-  }).startRTM(function (err, bot, payload) {
-    if (err) {
-      throw new Error(err)
-    }
+    console.log('Starting in single-team mode')
+    controller.spawn({
+            token: token
+        }).startRTM(function(err, bot, payload) {
+            if (err) {
+                throw new Error(err)
+            }
 
-    console.log('Connected to Slack RTM')
-  })
-// Otherwise assume multi-team mode - setup beep boop resourcer connection
+            console.log('Connected to Slack RTM')
+        })
+        // Otherwise assume multi-team mode - setup beep boop resourcer connection
 } else {
-  console.log('Starting in Beep Boop multi-team mode')
-  require('beepboop-botkit').start(controller, { debug: true })
+    console.log('Starting in Beep Boop multi-team mode')
+    require('beepboop-botkit').start(controller, {
+        debug: true
+    })
 }
 
-controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
+controller.on('bot_channel_join', function(bot, message) {
+    bot.reply(message, "I'm here!")
 })
 
-controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
+controller.hears(['hello', 'hi'], ['direct_mention'], function(bot, message) {
+    bot.reply(message, 'Hello.')
 })
 
-controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-  bot.reply(message, 'It\'s nice to talk to you directly.')
+controller.hears(['hello', 'hi'], ['direct_message'], function(bot, message) {
+    bot.reply(message, 'Hello.')
+    bot.reply(message, 'It\'s nice to talk to you directly.')
 })
 
-controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:')
+controller.hears('.*', ['mention'], function(bot, message) {
+    bot.reply(message, 'You really do care about me. :heart:')
 })
 
-controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help)
+controller.hears('help', ['direct_message', 'direct_mention'], function(bot, message) {
+    var help = 'I will respond to the following messages: \n' +
+        '`bot hi` for a simple message.\n' +
+        '`bot attachment` to see a Slack attachment message.\n' +
+        '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
+        '`bot help` to see this again.'
+    bot.reply(message, help)
 })
 
+/*
 controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
+
   var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
   var attachments = [{
     fallback: text,
@@ -63,123 +67,101 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
     text: text,
     color: '#7CD197'
   }]
+*/
 
-  controller.hears(['approval02'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [
-      {
-		 "color": "#2ab27b",
-         "fields":[
-            {
-               "title":"Report Name",
-               "value":"Trip to Seattle",
-               "short":true
-            },
-            {
-               "title":"Date",
-               "value":"7/1/2016",
-               "short":true
-            },
-            {
-               "title":"Amount",
-               "value":"$9,000.00",
-               "short":true
-            },
-				{
-               "value":":white_check_mark: Approved"
+controller.hears(['approval02'], ['direct_message', 'direct_mention'], function(bot, message) {
+    var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+    var attachments = [{
+        "color": "#2ab27b",
+        "fields": [{
+            "title": "Report Name",
+            "value": "Trip to Seattle",
+            "short": true
+        }, {
+            "title": "Date",
+            "value": "7/1/2016",
+            "short": true
+        }, {
+            "title": "Amount",
+            "value": "$9,000.00",
+            "short": true
+        }, {
+            "value": ":white_check_mark: Approved"
 
-            }
-         ]
-	  },{
-         "fields":[
-            {
-               "title":"Report Name",
-               "value":"Trip to Seattle",
-               "short":true
-            },
-            {
-               "title":"Date",
-               "value":"7/1/2016",
-               "short":true
-            },
-            {
-               "title":"Amount",
-               "value":"$9,000.00",
-               "short":true
-            },
-            {
-               "title":"Status",
-               "value":"Pending Review",
-               "short":true
-            }
-         ],
-         "actions":[
-            {
-               "name":"Approve",
-               "text":"Approve",
-               "type":"button",
-               "style":"primary",
-               "value":"approve"
-            },
-            {
-               "name":"Reject",
-               "text":"Reject",
-               "type":"button",
-               "style":"danger",
-               "value":"Reject"
-            }
-         ]
-      },{
-         "fields":[
-            {
-               "title":"Report Name",
-               "value":"Trip to Seattle",
-               "short":true
-            },
-            {
-               "title":"Date",
-               "value":"7/1/2016",
-               "short":true
-            },
-            {
-               "title":"Amount",
-               "value":"$9,000.00",
-               "short":true
-            },
-            {
-               "title":"Status",
-               "value":"Pending Review",
-               "short":true
-            }
-         ],
-         "actions":[
-            {
-               "name":"Approve",
-               "text":"Approve",
-               "type":"button",
-               "style":"primary",
-               "value":"approve"
-            },
-            {
-               "name":"Reject",
-               "text":"Reject",
-               "type":"button",
-               "style":"danger",
-               "value":"Reject"
-            }
-         ]
-      }, {
-      	"text": "<https://concur.com|View In Concur>"
-      }
-   ]
+        }]
+    }, {
+        "fields": [{
+            "title": "Report Name",
+            "value": "Trip to Seattle",
+            "short": true
+        }, {
+            "title": "Date",
+            "value": "7/1/2016",
+            "short": true
+        }, {
+            "title": "Amount",
+            "value": "$9,000.00",
+            "short": true
+        }, {
+            "title": "Status",
+            "value": "Pending Review",
+            "short": true
+        }],
+        "actions": [{
+            "name": "Approve",
+            "text": "Approve",
+            "type": "button",
+            "style": "primary",
+            "value": "approve"
+        }, {
+            "name": "Reject",
+            "text": "Reject",
+            "type": "button",
+            "style": "danger",
+            "value": "Reject"
+        }]
+    }, {
+        "fields": [{
+            "title": "Report Name",
+            "value": "Trip to Seattle",
+            "short": true
+        }, {
+            "title": "Date",
+            "value": "7/1/2016",
+            "short": true
+        }, {
+            "title": "Amount",
+            "value": "$9,000.00",
+            "short": true
+        }, {
+            "title": "Status",
+            "value": "Pending Review",
+            "short": true
+        }],
+        "actions": [{
+            "name": "Approve",
+            "text": "Approve",
+            "type": "button",
+            "style": "primary",
+            "value": "approve"
+        }, {
+            "name": "Reject",
+            "text": "Reject",
+            "type": "button",
+            "style": "danger",
+            "value": "Reject"
+        }]
+    }, {
+        "text": "<https://concur.com|View In Concur>"
+    }]
 
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
-  })
+    bot.reply(message, {
+        attachments: attachments
+    }, function(err, resp) {
+        console.log(err, resp)
+    })
 })
 
-controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+controller.hears('.*', ['direct_message', 'direct_mention'], function(bot, message) {
+    bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
 })
