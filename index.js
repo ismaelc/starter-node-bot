835d459eee7c56534c2fe405265d6c977dbe1f5e
ismaelc <chrisi@13MBP-11334.attlocal.net>
2016-07-15 09:52:05 -0700

    test

--- index.js
+++ index.js
@@ -103,6 +103,61 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function(
     })
 })
 
+controller.hears(['comic'], ['direct_message', 'direct_mention'], function(bot, message) {
+    var text = 'New comic book alert!'
+    var attachments = [
+        {
+            "title": "The Further Adventures of Slackbot",
+            "fields": [
+                {
+                    "title": "Volume",
+                    "value": "1",
+                    "short": true
+                },
+                {
+                    "title": "Issue",
+                    "value": "3",
+            "short": true
+                }
+            ],
+            "author_name": "Stanford S. Strickland",
+            "author_icon": "https://api.slack.com/img/api/homepage_custom_integrations-2x.png",
+            "image_url": "http://i.imgur.com/OJkaVOI.jpg?1"
+        },
+        {
+            "title": "Synopsis",
+            "text": "After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy..."
+        },
+        {
+            "fallback": "Would you recommend it to customers?",
+            "title": "Would you recommend it to customers?",
+            "callback_id": "comic_1234_xyz",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "recommend",
+                    "text": "Recommend",
+                    "type": "button",
+                    "value": "recommend"
+                },
+                {
+                    "name": "no",
+                    "text": "No",
+                    "type": "button",
+                    "value": "bad"
+                }
+            ]
+        }
+    ]
+
+    bot.reply(message, {
+        attachments: attachments
+    }, function(err, resp) {
+        console.log(err, resp)
+    })
+})
+
 controller.hears(['approval02'], ['direct_message', 'direct_mention'], function(bot, message) {
     //var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
     var attachments = [{
